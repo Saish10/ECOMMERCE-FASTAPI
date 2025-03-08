fdfe76a6250e17b756d9700c0da5dd9475258f9d
Saish Naik <sn.saishnaik@gmail.com>
2025-03-09 00:30:10 +0530

    updated sql script

--- create_tables.sql
+++ create_tables.sql
@@ -1,5 +1,5 @@
-CREATE TABLE Customers (
-    customer_id SERIAL PRIMARY KEY,
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
@@ -11,34 +11,30 @@ CREATE TABLE Customers (
 );
 
 -- Create Products Table
-CREATE TABLE Products (
-    product_id SERIAL PRIMARY KEY,
-    product_name VARCHAR(100) NOT NULL,
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
     description TEXT,
     category VARCHAR(50),
     price DECIMAL(10,2) NOT NULL,
     stock_quantity INT NOT NULL
-    -- price DECIMAL(10,2) NOT NULL CHECK (price >= 0),
-    -- stock_quantity INT NOT NULL CHECK (stock_quantity >= 0)
 );
 
 -- Create Orders Table
-CREATE TABLE Orders (
-    order_id SERIAL PRIMARY KEY,
-    customer_id INT NOT NULL REFERENCES Customers(customer_id) ON DELETE CASCADE,
-    order_date DATE NOT NULL,
+CREATE TABLE orders (
+    id SERIAL PRIMARY KEY,
+    customer_id INT NOT NULL REFERENCES customers(id) ON DELETE CASCADE,
+    date DATE NOT NULL,
     total_amount DECIMAL(10,2) NOT NULL,
-    -- total_amount DECIMAL(10,2) NOT NULL CHECK (total_amount >= 0),
     status VARCHAR(50) NOT NULL
 );
 
 -- Create Order_Items Table
-CREATE TABLE Order_Items (
-    order_item_id SERIAL PRIMARY KEY,
-    order_id INT NOT NULL REFERENCES Orders(order_id) ON DELETE CASCADE,
-    product_id INT NOT NULL REFERENCES Products(product_id),
+CREATE TABLE order_items (
+    id SERIAL PRIMARY KEY,
+    order_id INT NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
+    product_id INT NOT NULL REFERENCES products(id),
     quantity INT NOT NULL,
     price DECIMAL(10,2)
-    -- quantity INT NOT NULL CHECK (quantity > 0),
-    -- price DECIMAL(10,2) NOT NULL CHECK (price >= 0)
+
 );
